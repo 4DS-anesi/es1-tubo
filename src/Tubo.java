4a296a16050c51ae0e3623483c26f79ca30a2772
christian.anesi <christian.anesi@buonarroti.tn.it>
2023-09-23 06:52:22 +0200

    feat: Aggiunto metodo calcolo superfice

--- src/Tubo.java
+++ src/Tubo.java
@@ -10,4 +10,8 @@ public class Tubo {
     public float volume () {
         return (float) (Math.PI*Math.pow(r, 2)*h);
     }
+
+    public float superfice () {
+        return (float) ((2*Math.PI*r*h)+(2*Math.PI*Math.pow(r, 2)));
+    }
 }
