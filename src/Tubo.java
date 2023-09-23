a5b16a3a910754aa7f9eca0ff3e3949d3ba2ea49
christian.anesi <christian.anesi@buonarroti.tn.it>
2023-09-23 06:56:32 +0200

    feat: Aggiunto metodo set  per controllo e inserimento di r

--- src/Tubo.java
+++ src/Tubo.java
@@ -2,9 +2,17 @@ public class Tubo {
 
     private float r, h; //r=raggio l=lunghezza
 
-    public Tubo(float r, float h) {
-        this.r = r;
+    public Tubo(float r, float h) throws Exception {
+        setR(r);
         this.h = h;
+    }
+
+    public void setR(float r) throws Exception {
+        if (r > 0) {
+            this.r=r;
+        } else {
+            throw new Exception("Errore! Raggio non valido");
+        }
     }
 
     public float volume () {
