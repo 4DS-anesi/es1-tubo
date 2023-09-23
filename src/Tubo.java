1bd6ce5b85eacf2e04e16259d4b1d12ccc0d52d3
christian.anesi <christian.anesi@buonarroti.tn.it>
2023-09-23 06:48:31 +0200

    fix: sostituito variabile l con h

--- src/Tubo.java
+++ src/Tubo.java
@@ -1,13 +1,13 @@
 public class Tubo {
 
-    private float r, l; //r=raggio l=lunghezza
+    private float r, h; //r=raggio l=lunghezza
 
-    public Tubo(float r, float l) {
+    public Tubo(float r, float h) {
         this.r = r;
-        this.l = l;
+        this.h = h;
     }
 
     public float volume () {
-        return (float) (Math.PI*Math.pow(r, 2)*l);
+        return (float) (Math.PI*Math.pow(r, 2)*h);
     }
 }
