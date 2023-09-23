de80509a03e38a68abeb60e3b35ffc1fbb66723c
christian.anesi <christian.anesi@buonarroti.tn.it>
2023-09-23 07:15:38 +0200

    test: Aggiunto menu per test nel main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,58 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+        Tubo tubo;
+
+        int scelta=0;
+        boolean flag=false;
+        float r, h;
+
+        do {
+            System.out.println("Inserire raggio tubo: ");
+            r=input.nextFloat();
+            System.out.println("Inserire altezza tubo: ");
+            h=input.nextFloat();
+
+            try {
+                tubo = new Tubo(r, h);
+                flag=true;
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        } while (flag==false);
+
+
+
+        do{
+            System.out.println("\nMenu TEST\n1. Calcolo volume\n2. Calcolo superfice\n3. Modifica il raggio\n4. Modifica l'altezza\n5. Visualizza dati tubo\n0. Esci");
+            System.out.println("Scelta: ");
+            scelta=input.nextInt();
+            switch (scelta){
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 0:
+                    System.out.println("\nTerminazione programma");
+                    break;
+                default:
+                    System.out.println("\nScelta non valida");
+                    break;
+            }
+        }while (scelta != 0);
     }
 }
